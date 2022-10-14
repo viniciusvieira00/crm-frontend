e9b5610c00f236b57da47fe76bb71f1ae016cc80
viniciusvieira00 <vncvieira00@gmail.com>
2022-10-14 19:18:49 -0300

    foi

--- src/utils/apiKanban.js
+++ src/utils/apiKanban.js
@@ -3,5 +3,6 @@ import react from 'react';
 
 
 export default axios.create( {
-    baseURL: `http://localhost:8001/api/v1/`
+    // baseURL: `http://localhost:8001/api/v1/`
+    baseURL: `https://management-api-br.herokuapp.com/`
 });
